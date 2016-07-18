210c75cc23b953b3f69b60895ed0bf6e71703b7e
delucks <jamie.delucks@gmail.com>
2016-07-17 20:45:31 -0400

    Created some more tests, mocking the class initialization

--- test_gtd.py
+++ test_gtd.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3.2
 from gtd import parse_configuration, GTD_Controller, GTD_Display
+
 import trello
 import pytest
+# TODO upgrade my version of python and get rid of this
+import mock
 
 def test_parse_configuration():
     # open and parse our example configs
@@ -26,22 +30,43 @@ def test_parse_configuration():
         }
     }
 
-class TestGTDControllerInitialization:
-    def test_initialize_trello(self):
-        '''initialize the trello client as we normally would
-        '''
-        configs = parse_configuration()
+def test_bad_trello_key():
+    '''test the case where we pass invalid trello creds
+    '''
+    configs = parse_configuration('gtd.yaml.example')
+    with pytest.raises(trello.Unauthorized):
         g = GTD_Controller(configs, None)
-        # make sure the trello client and target board were bound
-        assert type(g.trello) == trello.TrelloClient
-        assert type(g.board) == trello.Board
-        # make sure all the list IDs are set up correctly
-        assert type(g.lists) == dict
-        assert all(isinstance(e, str) for _, e in g.lists.items())
 
-    def test_bad_trello_key(self):
-        '''test the case where we pass invalid trello creds
-        '''
-        configs = parse_configuration('gtd.yaml.example')
-        with pytest.raises(trello.Unauthorized):
-            g = GTD_Controller(configs, None)
+@pytest.fixture(scope='session')
+def configs():
+    return parse_configuration()
+
+@pytest.fixture
+def uninitialized(configs):
+    '''a GTD_Controller that hasn't been __init__'d yet
+    '''
+    with mock.patch.object(GTD_Controller, '__init__', return_value=None) as mock_init:
+        return GTD_Controller(configs, None)
+
+def test_initialize_trello(uninitialized, configs):
+    with pytest.raises(AttributeError):
+        getattr(uninitialized, 'trello')
+    client = uninitialized.initialize_trello(configs)
+    assert type(client) == trello.TrelloClient
+    assert client.api_key == configs['trello']['api_key']
+    assert client.oauth.client.client_key == configs['trello']['api_key']
+    assert client.api_secret == configs['trello']['api_secret']
+    assert client.oauth.client.client_secret == configs['trello']['api_secret']
+    
+def test_validate_config(uninitialized, configs):
+    uninitialized.trello = uninitialized.initialize_trello(configs)
+    board = uninitialized.validate_config(configs)
+    # make sure the board was initialized and bound correctly
+    assert type(board) == trello.Board
+    assert board.name.decode('utf-8') == configs['board_name']
+    assert board == uninitialized.board
+    # make sure the lists were found and registered
+    assert type(uninitialized.lists) == dict
+    for label, id in uninitialized.lists.items():
+        list_name = configs['list_names'][label]
+        assert uninitialized._find_list_id(board, list_name) == id
