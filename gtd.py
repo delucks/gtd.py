a0bc94b223b852d34ffabf8ba082aab82d93c110
delucks <me@jamesluck.com>
2016-12-23 08:32:50 +0000

    Added pretty colors and argparsing

--- gtd.py
+++ gtd.py
@@ -1,27 +1,37 @@
 '''Incremental development is a thing dude
 TODOs:
-- Arg parsing
 - Option to work on a list other than "Inbound"
 - "Daily review" mode where you review the active and blocked ones as well
-- Arg to invert the list (most recent first)
-- Arg to filter the list to only items matching a regex
 '''
 import sys
 import logging
 import readline
 import datetime
-#import argparse
+import argparse
 from trello import TrelloClient
 import yaml
 
+
+class Colors:
+    esc = '\033'
+    black = esc + '[0;30m'
+    red = esc + '[0;31m'
+    green = esc + '[0;32m'
+    yellow = esc + '[0;33m'
+    blue = esc + '[0;34m'
+    purple = esc + '[0;35m'
+    cyan = esc + '[0;36m'
+    white = esc + '[0;37m'
+    reset = esc + '[0m'
+
 __version__ = '0.0.2'
-__banner__ = '''Welcome to
-  __|_ _| ._    
- (_||_(_|o|_)\/ 
-  _|      |  /  
+__banner__ = '''Welcome to{1.blue}
+  __|_ _| ._
+ (_||_(_|o|_)\/
+  _|      |  /{1.reset} 
 version {0}
 by delucks
-'''.format(__version__)
+'''.format(__version__, Colors)
 
 
 def parse_configuration(configfile='gtd.yaml'):
@@ -51,21 +61,29 @@ def filter_them(iterable, name):
     return [b for b in iterable if b.name == bytes(name, 'utf8')][0]
 
 
+def _colorize(lbl, msg, colorstring=Colors.blue):
+    return '  {0}{1}{2} {3}'.format(colorstring, lbl, Colors.reset, msg)
+
+
 def display_card(card):
     created = card.create_date
-    print('Card {0}'.format(card.id))
-    print('  Name: {0}'.format(card.name.decode('utf8')))
-    print('  Created on: {0} ({1})'.format(created, created.timestamp()))
-    print('  Age: {0}'.format(datetime.datetime.now(datetime.timezone.utc) - created))
+    print('{1.red}Card {0}{1.reset}'.format(card.id, Colors))
+    print(_colorize('Name:', card.name.decode('utf8')))
+    print(_colorize('Created on:', '{0} ({1})'.format(created, created.timestamp())))
+    print(_colorize('Age:', datetime.datetime.now(datetime.timezone.utc) - created))
     if card.labels:
-        print('  Labels: {0}'.format(','.join([l.name.decode('utf8') for l in card.labels])))
+        print(_colorize('Labels:', ','.join([l.name.decode('utf8') for l in card.labels])))
     if card.get_attachments():
-        print('  Attachments: {0}'.format(','.join([a['name'] for a in card.get_attachments()])))
+        print(_colorize('Attachments:', ','.join([a['name'] for a in card.get_attachments()])))
     if card.due:
         # TODO add red for overdue cards
         diff = card.due_date - datetime.datetime.now(datetime.timezone.utc)
-        print('  Due: {0}'.format(card.due_date))
-        print('  (Remaining): {0}'.format(diff))
+        if diff < datetime.timedelta(0):
+            display = Colors.red
+        else:
+            display = Colors.green
+        print(_colorize('Due:', card.due_date, display))
+        print(_colorize('Remaining:', diff, display))
 
 
 def prompt_for_user_choice(iterable):
@@ -125,19 +143,33 @@ def make_readable(object_grouping):
 
 
 def main():
-    #p = argparse.ArgumentParser('gtd.py version {0}'.format(__version__))
     config_properties = parse_configuration()
+    p = argparse.ArgumentParser(description='gtd.py version {0}'.format(__version__))
+    #p.add_argument('-r', '--reverse', help='process the list of cards in reverse', action='store_true')
+    #p.add_argument('-m', '--match', help='provide a regex to filter the card names on')
+    p.add_argument('-d', '--display', help='just show em', action='store_true')
+    p.add_argument('-l', '--list', help='list to use', default=config_properties['list_names']['incoming'])
+    p.add_argument('--lists', help='dump lists', action='store_true')
+    args = p.parse_args()
     trello = initialize_trello(config_properties)
-
     main_board = filter_them(trello.list_boards(), config_properties['board_name'])
-    inbound_list = filter_them(main_board.get_lists('open'), config_properties['list_names']['incoming'])
-    other_lists = main_board.get_lists('open')
-    all_labels = main_board.get_labels()
-
-    label_lookup = make_readable(all_labels)
-    list_lookup = make_readable(other_lists)
+    inbound_list = filter_them(main_board.get_lists('open'), args.list)
 
     print(__banner__)
+
+    if args.display:
+        for card in inbound_list.list_cards():
+            display_card(card)
+        sys.exit(0)
+    elif args.lists:
+        for l in main_board.get_lists('open'):
+            print(l.name.decode('utf8'))
+        sys.exit(0)
+
+    other_lists = main_board.get_lists('open')
+    all_labels = main_board.get_labels()
+    label_lookup = make_readable(all_labels)
+    list_lookup = make_readable(other_lists)
     for card in inbound_list.list_cards():
         display_card(card)
         keep =  prompt_for_confirmation('Should we keep it? (Y/n) ', True)
